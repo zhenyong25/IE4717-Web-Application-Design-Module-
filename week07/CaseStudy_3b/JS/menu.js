4a50b65217610e559d719d8a4c4d90528defdc75
zhenyong25 <zhenyong25@gmail.com>
2022-10-04 03:11:22 +0800

    Updated JS Files

---
+++ week07/CaseStudy_3b/JS/menu.js
@@ -0,0 +1,21 @@
+function just_java(){
+
+    var java = document.getElementById("java").value
+
+    return ; 
+}
+
+function cafe_au_laut(){
+
+    return ; 
+}
+
+function cappuccino(){
+
+    return ; 
+}
+
+function total_price(){
+
+    return ; 
+} 
